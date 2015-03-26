a8cab8c953fd5cf00a34f1d9a56f743fee34f599
otabat <otabat@gmail.com>
2015-03-26 13:43:46 +0900

    Added leiningen project file.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject rxclojure "1.0.1-dev.3+0.x-SNAPSHOT"
+  :description "Unofficial build of rxclojure."
+  :url "https://github.com/otabat/RxClojure"
+  :license {:name "The Apache Software License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.reactivex/rxjava "1.0.8"]])
